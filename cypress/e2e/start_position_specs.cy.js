7e0cbb515951c2a7c38b602a21ad21d7b0ff4a65
Abraham Azulay <abraham.azulay@ufl.edu>
2023-05-01 17:20:01 -0400

    Cypress test correction

--- cypress/e2e/start_position_specs.cy.js
+++ cypress/e2e/start_position_specs.cy.js
@@ -1,14 +1,15 @@
 describe('Start Position Button', () => {
 
   beforeEach(() => {
-    cy.visit('http://localhost:3000'); // Add this line to visit your application
+    cy.visit('http://localhost:3000'); 
   });
+
   it('should reset the chessboard to the starting position', () => {
     // Click the start position button
     cy.get('#start-position-button').click();
 
     // Check if the chessboard is reset to the initial position
-    cy.get('.square img').each(($img, index) => {
+    cy.get('.square').each(($square, index) => {
       const initialPosition = [
         'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r',
         'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p',
@@ -25,13 +26,13 @@ describe('Start Position Button', () => {
         const expectedPiece = piece.toUpperCase();
         const expectedColor = piece === piece.toUpperCase() ? 'w' : 'b';
 
-        // Check if the src attribute contains the expected piece and color information
-        cy.wrap($img)
+        cy.wrap($square)
+          .find('img')
           .invoke('attr', 'src')
           .should('contain', expectedColor)
           .and('contain', expectedPiece);
       } else {
-        cy.wrap($img).should('not.exist');
+        cy.wrap($square).find('img').should('not.exist');
       }
     });
   });
